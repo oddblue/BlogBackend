18299d2ca5bcf449c63e15316e61ffabe6dae06d
oddblue <1850528268@qq.com>
2025-06-05 19:41:24 +0800

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');// 日志中间件
-//const helmet = require('helmet');// 安全中间件
+const helmet = require('helmet');// 安全中间件
 const connectDB = require('./config/db'); // 引入数据库连接
 
 const app = express();
@@ -11,20 +11,21 @@ const port = process.env.PORT || 3000;
 
 // 中间件
 app.use(morgan('dev')); // 日志记录中间件
-//app.use(helmet()); // 安全中间件
+app.use(helmet()); // 安全中间件
 app.use(cors()); // 允许跨域
-/*
+
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://your-frontend.com'],
+  origin: ['https://blog-eosin-iota.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
-}));*/
-/*const rateLimit = require('express-rate-limit');
+}));
+
+const rateLimit = require('express-rate-limit');
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000, // 15 分钟
   max: 100, // 每个 IP 每窗口期限制 100 次请求
   message: { error: '请求过于频繁，请稍后再试' }
-}));*/
+}));
 
 app.use(express.json({ limit: '10mb' }));// 解析 JSON 请求体,限制为 10MB
 // 解析 URL 编码的请求体,限制为 10MB
